3194fdd98105931bacfce40614449830f2568456
David Conner <rdconner@us.ibm.com>
2017-03-15 08:38:25 -0500

    create post data json and populate templates with it

--- server/routes/post-routes.js
+++ server/routes/post-routes.js
@@ -2,29 +2,46 @@ import express from 'express';
 import fs from 'fs';
 import fm from 'front-matter';
 import {markdown as md} from 'markdown';
-import nav from '../nav.js';
+const postData = require('../../src/data/post.json');
 const router = express.Router();
 
-const navigation = nav((nav) => {
 
   // ---------------------------------
   // -- create nav and post objects --
   // ---------------------------------
 
   //create navigation from list of tags
+  let navLinks = [];
+  let postList = []
   const sortedNav = [];
   const tagPost = {};
-  nav.nav.map((item, i) => {
+  Object.keys(postData).map((poster) => {
+    navLinks = navLinks.concat(postData[poster].tags);
+    postList = postList.concat(postData[poster]);
+  });
+  //remove dupes
+  navLinks = navLinks.reduce((a, b) => {
+    if (a.indexOf(b) < 0 ) a.push(b);
+    return a;
+  },[]);
+  //sort from newest to oldest
+  postList.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return ( dateA < dateB) ? 1 : (dateA > dateB) ? -1 : 0;
+  });
+  console.log("POST DATA:", navLinks);
+  navLinks.map((item, i) => {
     //create link object to be passed into nav array for ejs template
     const link = {
-      Link:`/${nav.nav[i]}`,
-      Text: `${nav.nav[i]}`
+      Link:`/${navLinks[i]}`,
+      Text: `${navLinks[i]}`
     }
     //push link object onto nav array
     sortedNav.push(link);
 
     tagPost[item] = [];
-    nav.list.map((post, index) => {
+    postList.map((post, index) => {
       if(post.tags.includes(item) ) {
         tagPost[item].push(post);
       }
@@ -40,7 +57,7 @@ const navigation = nav((nav) => {
 
     res.render('index', {
         nav: sortedNav,
-        list: nav.list
+        list: postList
     });
   });
 
@@ -53,27 +70,16 @@ const navigation = nav((nav) => {
           list: tagPost[req.params.tag]
       });
     }
-    res.redirect('/');
-
   });
 
   //render single post
   router.get('/post/:post', (req, res) => {
-    //get yaml front matter and body of post
-    fs.readFile(`./src/post/${req.params.post}`, 'utf8', (err, data) => {
-      if (err) {
-        res.redirect('/');
-      }
-      const content = fm(data);
-      const body = md.toHTML(content.body);
-      //once read render single post
-      res.render('post', {
-          nav: sortedNav,
-          post: content,
-          body: body
-      });
+    const post = postData[req.params.post + '.md'];
+    res.render('post', {
+      nav: sortedNav,
+      post: post,
+      body: post.body
     });
   });
-});
 
 module.exports = router;
