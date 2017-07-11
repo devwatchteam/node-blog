78ecdd671acd4a2d87da785833f6a0f316360748
David Conner <david@david-conner.com>
2017-07-10 21:33:59 -0500

    effin route regex

--- server/routes/post-routes.js
+++ server/routes/post-routes.js
@@ -13,39 +13,62 @@ _async_ (() => {
 
   // pages
   _await_ (fs.readdir(`./src/views/pages/`, (err, pages) => {
+
     //create links for pages navigation
     pages.map(page => {
       const pageLink = page.replace(/\.[^/.]+$/, "");
       router.get(`${process.env.npm_package_reponame}/${pageLink}.html`, (req, res) => {
-        console.log("PAGELINK: ", req.originalUrl);
+        console.log("PAGELINK: ", pageLink);
         res.sendFile(ROOT_DIR + `/${pageLink}.html`);
       });
 
     });
   }));
 
-  //render list from tags
-  router.get(`${process.env.npm_package_reponame}/:tag`, (req, res) => {
-    console.log("TAG: ", req.originalUrl);
-    res.sendFile(ROOT_DIR + `/${req.params.tag}/index.html`);
-  });
-
-  router.get(`${process.env.npm_package_reponame}/:tag/index-[0-9]*`, (req, res) => {
-    console.log("TAG-SUB: ", req.originalUrl);
-    res.sendFile(ROOT_DIR + `/${req.params.tag}/index.html`);
+  //Home page. render list from post directory
+  router.get(`${process.env.npm_package_reponame}`, (req, res) => {
+    console.log("NODE-BLOG: ", req.originalUrl);
+    res.sendFile(ROOT_DIR + `/index.html`);
   });
 
   //render single post
   router.get(`${process.env.npm_package_reponame}/post/:post`, (req, res) => {
-    console.log("POST: ", req.originalUrl);
-    res.sendFile(ROOT_DIR + `/post/${req.params.post}.html`);
+    console.log("POST: ", req.params.post);
+    res.sendFile(ROOT_DIR + `/post/${req.params.post}`);
   });
+  //
+  // router.get(`${process.env.npm_package_reponame}/:tag/index-[0-9]*.html`, (req, res) => {
+  //   console.log("TAG-SUB: ", req.originalUrl.replace(`${process.env.npm_package_reponame}`, ''));
+  //   res.sendFile(ROOT_DIR + `${req.originalUrl.replace(`${process.env.npm_package_reponame}`, '')}`);
+  // });
+  //
+  // router.get(`${process.env.npm_package_reponame}/index*-*[0-9]*.html`, (req, res) => {
+  //   console.log("INDEX-SUB: ", req.originalUrl.replace(`${process.env.npm_package_reponame}`, ''));
+  //   res.sendFile(ROOT_DIR + `${req.originalUrl.replace(`${process.env.npm_package_reponame}`, '')}`);
+  // });
+  //
+  //
 
   //Home page. render list from post directory
-  router.get(`/|/index.html`, (req, res) => {
-    console.log("ORIGNAL: ", req.originalUrl);
-    res.sendFile(ROOT_DIR + `/index.html`);
+  router.get(/(\process.env.npm_package_reponame)*\/(index)*-*[0-9]*.html/, (req, res) => {
+    console.log("ORIGNAL: ", req.originalUrl.replace(`${process.env.npm_package_reponame}`, ''));
+    res.sendFile(ROOT_DIR + req.originalUrl.replace(`${process.env.npm_package_reponame}`, ''));
   });
+
+  //render list from tags
+  router.get(`${process.env.npm_package_reponame}/:tag([a-zA-Z0-9]*(?!\/))`, (req, res) => {
+    console.log("TAG: ", req.originalUrl, req.params.tag);
+    res.sendFile(ROOT_DIR + `/${req.params.tag}/index.html`);
+  });
+
+
+
+
+  //Home page. render list from post directory
+  // router.get(`/|/index.html`, (req, res) => {
+  //   console.log("ORIGNAL: ", req.originalUrl);
+  //   res.sendFile(ROOT_DIR + `/index.html`);
+  // });
 })();
 
 
